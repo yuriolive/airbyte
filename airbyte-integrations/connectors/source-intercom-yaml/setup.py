7ac618a1508c43c122a7411f9e76daceb281e3c8
Oleksandr Bazarnov <oleksandr.bazarnov@globallogic.com>
2023-02-01 16:42:19 +0200

    initial commit

---
+++ airbyte-integrations/connectors/source-intercom-yaml/setup.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2022 Airbyte, Inc., all rights reserved.
+#
+
+
+from setuptools import find_packages, setup
+
+MAIN_REQUIREMENTS = [
+    "airbyte-cdk",
+]
+
+TEST_REQUIREMENTS = [
+    "pytest",
+    "pytest-mock",
+    "source-acceptance-test",
+]
+
+setup(
+    name="source_intercom_yaml",
+    description="Source implementation for Intercom Yaml.",
+    author="Airbyte",
+    author_email="contact@airbyte.io",
+    packages=find_packages(),
+    install_requires=MAIN_REQUIREMENTS,
+    package_data={"": ["*.json", "*.yaml", "schemas/*.json", "schemas/shared/*.json"]},
+    extras_require={
+        "tests": TEST_REQUIREMENTS,
+    },
+)
